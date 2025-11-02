d3f2de30d7dd170c083a39526ece21e622aaca02
shiva <saishivakrishna2018@gmail.com>
2025-11-02 23:49:35 +0530

    todo

---
+++ loto/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import {Temp} from "./components/Templete"
+
+function App() {
+ 
+
+  return (
+    <>
+    <center>
+    <h1 >TODO-TEXT</h1>
+    </center>
+     <Temp/>
+    </>
+  )
+}
+
+export default App
---
+++ loto/src/components/Templete.jsx
@@ -0,0 +1,70 @@
+import { Component } from "react";
+import './Templete.css'
+
+export class Temp extends Component{
+    constructor(){
+        super()
+        
+        this.state={list:[], text:"",toUpdateData:false,updatedData:"",index:null}
+    }
+    save=(e)=>{
+       this.setState({text:e.target.value})
+    }
+    button=()=>{
+        this.setState({list:[...this.state.list,this.state.text],text:""})
+    }
+    delete=(index)=>{
+        let l=[...this.state.list]
+        l.splice(index,1)
+        this.setState({list:l})
+
+    }
+   edit = (i) => {
+        this.setState({
+            toUpdateData: true,
+            updatedData: this.state.list[i], 
+            index: i
+        })
+    }
+
+    update=()=>{
+        let l=[...this.state.list]
+        l[this.state.index]=this.state.updatedData;
+        this.setState({
+            list:l,
+            updatedData:"",
+            index:null,
+            toUpdateData:false
+        })
+
+
+    }
+   
+
+    render(){
+        return(
+            <div style={{display:"flex"}}>
+            <div className="inner">
+                <input type="text" onChange={(e)=>this.save(e)}/>
+                <button className="submit" onClick={this.button}>Add</button>
+                <div>
+                 {this.state.list.map((data,index)=>
+                <div  className="inner1" key={index}>
+                    <h1 className="data">{data}</h1>
+                    <button className="edit" onClick={()=>this.edit(index)}>Edit</button>
+                    <button className="delete" onClick={()=>this.delete(index)}>Delete</button>
+
+                </div>
+                )}
+               
+                </div>
+                </div>
+                 <div className="update">
+                    {this.state.toUpdateData && <div> <input type="text" onChange={(e)=>this.setState({updatedData:e.target.value})} type="text" value={this.state.updatedData}/>
+                    <button onClick={this.update}>update</button> </div>}
+                    
+                </div>
+            </div>
+        )
+    }
+}
